f761b534e59652de6762f7b826fd2c23697e1344
Tyler Decker <tyler.decker@gmail.com>
2016-10-26 19:05:04 -0500

    All test now use test_helper

--- tests/board_properties.rs
+++ tests/board_properties.rs
@@ -1,32 +1,26 @@
 extern crate tic_tac_toe;
-use tic_tac_toe::game_board::game_board::GameBoard;
+mod test_helper;
+use test_helper::*;
 use tic_tac_toe::game_board::properties_traits::HasProperties;
-
-static FILL: usize = 0;
-
-fn board_3x3() -> GameBoard<usize> {
-    GameBoard::new(3, FILL)
-}
-
-fn board_4x4() -> GameBoard<usize> {
-    GameBoard::new(4, FILL)
-}
 
 #[test]
 fn trait_size_returns_9_on_3x3_board() {
     let test_board = board_3x3();
+
     assert_eq!(9, test_board.size());
 }
 
 #[test]
 fn row_size_returns_3_on_3x3_board() {
     let test_board = board_3x3();
+
     assert_eq!(3, test_board.row_size());
 }
 
 #[test]
 fn row_size_returns_4_on_4x4_board() {
     let test_board = board_4x4();
+
     assert_eq!(4, test_board.row_size());
 }
 
@@ -34,6 +28,7 @@ fn row_size_returns_4_on_4x4_board() {
 #[test]
 fn corners_returns_an_array_with_1_3_6_9_on_3x3_board() {
     let test_board = board_3x3();
+
     let expected = [1, 3, 6, 9];
     assert_eq!(expected, test_board.corners());
 }
@@ -41,6 +36,7 @@ fn corners_returns_an_array_with_1_3_6_9_on_3x3_board() {
 #[test]
 fn corners_returns_an_array_with_1_4_12_16_on_3x3_board() {
     let test_board = board_4x4();
+
     let expected = [1, 4, 12, 16];
     assert_eq!(expected, test_board.corners());
 }
--- tests/game_status.rs
+++ tests/game_status.rs
@@ -8,128 +8,128 @@ use tic_tac_toe::game_board::basic_board_traits::BasicBoard;
 #[test]
 fn game_status_returns_game_state_playing_on_empty_board() {
     let test_board = board_3x3();
-    let expected = GameState::Playing;
 
+    let expected = GameState::Playing;
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_tied_when_no_winner_and_board_is_full() {
     let mut test_board = board_3x3();
-    let expected = GameState::Tied;
 
     for num in 1..10 {
         test_board.set_space(num, num);
     }
 
+    let expected = GameState::Tied;
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_first_row_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
-    let expected = GameState::Winner;
 
     for index in 1..4 {
-        test_board.set_space(index, test_helper::TEST_MOVE);
+        test_board.set_space(index, TEST_MOVE);
     }
 
+    let expected = GameState::Winner;
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_second_row_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
-    let expected = GameState::Winner;
 
     for index in 4..7 {
-        test_board.set_space(index, test_helper::TEST_MOVE);
+        test_board.set_space(index, TEST_MOVE);
     }
 
+    let expected = GameState::Winner;
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_third_row_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
-    let expected = GameState::Winner;
 
     for index in 6..10 {
-        test_board.set_space(index, test_helper::TEST_MOVE);
+        test_board.set_space(index, TEST_MOVE);
     }
 
+    let expected = GameState::Winner;
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_diagnol_ascending_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
+
+    test_board.set_space(3, TEST_MOVE);
+    test_board.set_space(5, TEST_MOVE);
+    test_board.set_space(7, TEST_MOVE);
+
     let expected = GameState::Winner;
-
-    test_board.set_space(3, test_helper::TEST_MOVE);
-    test_board.set_space(5, test_helper::TEST_MOVE);
-    test_board.set_space(7, test_helper::TEST_MOVE);
-
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_diagnol_decending_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
+
+    test_board.set_space(1, TEST_MOVE);
+    test_board.set_space(5, TEST_MOVE);
+    test_board.set_space(9, TEST_MOVE);
+
     let expected = GameState::Winner;
-
-    test_board.set_space(1, test_helper::TEST_MOVE);
-    test_board.set_space(5, test_helper::TEST_MOVE);
-    test_board.set_space(9, test_helper::TEST_MOVE);
-
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_fist_col_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
+
+    test_board.set_space(1, TEST_MOVE);
+    test_board.set_space(4, TEST_MOVE);
+    test_board.set_space(7, TEST_MOVE);
+
     let expected = GameState::Winner;
-
-    test_board.set_space(1, test_helper::TEST_MOVE);
-    test_board.set_space(4, test_helper::TEST_MOVE);
-    test_board.set_space(7, test_helper::TEST_MOVE);
-
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_second_col_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
+
+    test_board.set_space(2, TEST_MOVE);
+    test_board.set_space(5, TEST_MOVE);
+    test_board.set_space(8, TEST_MOVE);
+
     let expected = GameState::Winner;
-
-    test_board.set_space(2, test_helper::TEST_MOVE);
-    test_board.set_space(5, test_helper::TEST_MOVE);
-    test_board.set_space(8, test_helper::TEST_MOVE);
-
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_third_col_is_all_the_same_and_not_blank() {
     let mut test_board = board_3x3();
+
+    test_board.set_space(3, TEST_MOVE);
+    test_board.set_space(6, TEST_MOVE);
+    test_board.set_space(9, TEST_MOVE);
+
     let expected = GameState::Winner;
-
-    test_board.set_space(3, test_helper::TEST_MOVE);
-    test_board.set_space(6, test_helper::TEST_MOVE);
-    test_board.set_space(9, test_helper::TEST_MOVE);
-
     assert_eq!(expected, test_board.game_status());
 }
 
 #[test]
 fn game_status_returns_game_state_winner_when_first_col_is_all_the_same_for_4x4() {
     let mut test_board = board_4x4();
+
+    test_board.set_space(1, TEST_MOVE);
+    test_board.set_space(5, TEST_MOVE);
+    test_board.set_space(9, TEST_MOVE);
+    test_board.set_space(13, TEST_MOVE);
+
     let expected = GameState::Winner;
-
-    test_board.set_space(1, test_helper::TEST_MOVE);
-    test_board.set_space(5, test_helper::TEST_MOVE);
-    test_board.set_space(9, test_helper::TEST_MOVE);
-    test_board.set_space(13, test_helper::TEST_MOVE);
-
     assert_eq!(expected, test_board.game_status());
 }
