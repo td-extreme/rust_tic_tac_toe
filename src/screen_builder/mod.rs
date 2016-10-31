6a8372c499651451077ea66f24d08eace8072c81
Tyler Decker <tyler.decker@gmail.com>
2016-10-30 20:28:30 -0500

    [WIP] Screen Builder

--- src/screen_builder/mod.rs
+++ src/screen_builder/mod.rs
@@ -1,32 +1,50 @@
-mod sprite_sheet;
-
+pub mod sprite_sheet;
+pub mod point_generator;
 
 use game_io::game_screen::sprite::Sprite;
 use game_io::game_screen::GameScreen;
 use game_io::game_screen::sprite::point::Point;
 use game_io::game_screen::sprite::color::Color;
-use game_io::screen_width;
-use game_io::screen_height;
 
-pub fn playing_screen() -> GameScreen {
-    let mut screen = GameScreen::new();
+pub struct ScreenBuilder {
+    screen_width: i32,
+    screen_height: i32,
+}
 
+impl ScreenBuilder {
+    pub fn new(screen_height: i32, screen_width: i32) -> ScreenBuilder {
+        ScreenBuilder {
+            screen_width: screen_width,
+            screen_height: screen_height,
+        }
+    }
 
-    let screen_width = screen_width();
-    let screen_height = screen_height();
+    pub fn playing_screen(&self) -> GameScreen {
+        let mut screen = GameScreen::new();
+        screen.add_sprite(self.title_bar_sprite());
+        screen.add_sprite(self.board_sprite());
+        screen
+    }
 
-    let board_width = sprite_sheet::board().width();
-    let board_height = sprite_sheet::board().height();
+    fn title_bar_sprite(&self) -> Sprite {
+        let sprite_data = sprite_sheet::title();
+        let sprite_width = sprite_sheet::title().width();
 
-    let board_x = (screen_height / 2) - (board_height / 2);
-    let board_y = (screen_width / 2) - (board_width / 2);
+        let sprite_y = point_generator::center_side(sprite_width, self.screen_width);
+        let point = Point::new(5, sprite_y);
 
-    let point = Point::new(board_x, board_y);
+        Sprite::new(point, Color::WhiteOnBlue, sprite_data)
+    }
 
-    let board_sprite = Sprite::new(point, Color::WhiteOnBlue, sprite_sheet::board());
+    fn board_sprite(&self) -> Sprite {
+        let board_width = sprite_sheet::board().width();
+        let board_height = sprite_sheet::board().height();
 
-    screen.add_sprite(board_sprite);
+        let point = point_generator::center(board_height,
+                                                  board_width,
+                                                  self.screen_height,
+                                                  self.screen_width);
 
-    screen
-
+        Sprite::new(point, Color::WhiteOnBlue, sprite_sheet::board())
+    }
 }
---
+++ tests/point_generator.rs
@@ -0,0 +1,15 @@
+extern crate tic_tac_toe;
+use tic_tac_toe::game_io::game_screen::sprite::point::Point;
+use tic_tac_toe::screen_builder::point_generator;
+
+#[test]
+fn center_sprite_on_screen_returns_a_point_of_25_50_for_sprite_50x100_on_100_200_screen() {
+    let point = point_generator::center(50, 100, 100, 200);
+    let expected = Point::new(25, 50);
+    assert_eq!(expected, point);
+}
+
+#[test]
+fn center_side_returns_50_for_100_200() {
+    assert_eq!(50, point_generator::center_side(100, 200));
+}
